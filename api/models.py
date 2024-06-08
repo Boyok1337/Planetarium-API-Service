996a23a7350a6c0db05ff62626b685a2063fd613
Andrii <andreboiko1@gmail.com>
2024-06-08 12:33:42 +0300

    update models: AstronomyShow, Ticket

--- api/models.py
+++ api/models.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models import (
     DateTimeField,
-    ForeignKey
+    ForeignKey, UniqueConstraint
 )
 
+from api.validators import validate_show_time
 
 User = get_user_model()
 
@@ -20,7 +22,7 @@ class ShowTheme(models.Model):
 
 
 class AstronomyShow(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, unique=True)
     description = models.TextField()
     show_theme = models.ManyToManyField(
         ShowTheme,
@@ -54,9 +56,33 @@ class ShowSession(models.Model):
     def __str__(self):
         return self.astronomy_show.title
 
+    def clean(self):
+        super().clean()
+        qs = ShowSession.objects.all()
+        validate_show_time(
+            show_time=self.show_time,
+            astronomy_show=self.astronomy_show,
+            planetarium_dome=self.planetarium_dome,
+            qs=qs,
+            instance=self
+        )
+
+    def save(self, *args, **kwargs) -> None:
+        self.full_clean()
+        return super().save(*args, **kwargs)
+
+    def get_available_seats(self):
+        taken_seats = self.tickets.count()
+        available_seats = self.planetarium_dome.capacity - taken_seats
+        return available_seats
+
+    @property
+    def show_time_formatted(self):
+        return self.show_time.strftime('%Y-%m-%d %H:%M')
+
 
 class PlanetariumDome(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     rows = models.PositiveIntegerField()
     seats_in_row = models.PositiveIntegerField()
 
@@ -83,8 +109,43 @@ class Ticket(models.Model):
         "Reservation",
         on_delete=models.SET_NULL,
         related_name="tickets",
-        null=True
+        null=True,
+        blank=True
     )
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=("show_session", "row", "seat"), name="unique_ticket")
+        ]
+        ordering = ("-reservation__created_at",)
+
+    def __str__(self):
+        return (
+            f"{self.show_session.astronomy_show.title}, "
+            f"row: {self.row} - "
+            f"seat: {self.seat} - "
+            f"reservation: {self.reservation.user.username}"
+        )
+
+    @staticmethod
+    def validate_seat_and_row(seat: int, row: int, num_seats: int, num_rows: int) -> None:
+        if seat > num_seats or seat < 0:
+            raise ValidationError("Invalid seat")
+
+        if row > num_rows or row < 0:
+            raise ValidationError("Invalid row")
+
+    def clean(self) -> None:
+        Ticket.validate_seat_and_row(
+            seat=self.seat,
+            row=self.row,
+            num_seats=self.show_session.planetarium_dome.seats_in_row,
+            num_rows=self.show_session.planetarium_dome.rows
+        )
+
+    def save(self, *args, **kwargs) -> None:
+        super().full_clean()
+        return super().save(*args, **kwargs)
 
 
 class Reservation(models.Model):
