5185fd76d068f0fcea63a1b4ce3c3bcb8b046770
Andrii <andreboiko1@gmail.com>
2024-06-08 12:32:43 +0300

    add validators for show_time

---
+++ api/validators.py
@@ -0,0 +1,17 @@
+from datetime import timedelta
+from django.utils import timezone
+from django.core.exceptions import ValidationError
+
+
+def validate_show_time(show_time, astronomy_show, planetarium_dome, qs, instance=None):
+    if show_time <= timezone.now():
+        raise ValidationError("Show time must be provided in the future")
+
+    qs = qs.exclude(pk=instance.pk if instance else None)
+    qs = qs.filter(
+        astronomy_show=astronomy_show,
+        planetarium_dome=planetarium_dome,
+    )
+    one_hour = timedelta(hours=1)
+    if qs.filter(show_time__gte=show_time-one_hour, show_time__lt=show_time).exists():
+        raise ValidationError("Show time must be at least 1 hour apart from the previous show time.")
