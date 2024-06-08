4251412dcf10bea0e14b08815dcc705f9701ef47
Andrii <andreboiko1@gmail.com>
2024-06-08 12:34:07 +0300

    register new urls

--- api/urls/planetarium_urls.py
+++ api/urls/planetarium_urls.py
@@ -2,13 +2,15 @@ from django.urls import path, include
 from rest_framework import routers
 
 from api.views.planetarium_views import ShowThemeViewSet, AstronomyShowViewSet, ShowSessionViewSet, \
-    PlanetariumDomeViewSet
+    PlanetariumDomeViewSet, TicketViewSet, ReservationViewSet
 
 router = routers.DefaultRouter()
 router.register('show_theme', ShowThemeViewSet, basename='show-theme')
 router.register('astronomy_show', AstronomyShowViewSet, basename='astronomy-show')
 router.register('show_session', ShowSessionViewSet, basename="show-session")
 router.register('planetarium_dome', PlanetariumDomeViewSet, basename='planetarium-dome')
+router.register('ticket', TicketViewSet, basename='ticket')
+router.register('reservation', ReservationViewSet, basename="reservation")
 
 urlpatterns = [
     path('', include(router.urls)),
